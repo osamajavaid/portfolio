9aa3a1562cd2dcd5bfccc6fc14d64740d811ed43
osamajavaid <18pwcse1638@uetpeshawar.edu.pk>
2022-12-31 17:07:46 +0500

    layout working

---
+++ components/Intro/Intro.jsx
@@ -0,0 +1,307 @@
+// import osama from 'images/osama.jpg'
+import Image from 'next/image'
+import { FaFacebook, FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
+const Intro = () => {
+    return (
+        <div className="h-full overflow-hidden relative bg-[#20202A] shadow-2xl flex flex-col">
+
+            <div className="headerr absolute bg-[#20202A] bg-opacity-95 inset-y-0 h-48 top-0 flex items-center justify-center w-full flex-col px-4 gap-y-4">
+                <img className='w-20 h-20 rounded-full' src='images/osama.jpg' alt="" />
+                <div className="flex flex-col items-center justify-center">
+                    <span className='text-white text-sm font-bold break-normal'>Muhammad Osama Javaid </span>
+                    <span className='text-xs text-gray-600 text-center'>MERN Stack Develoer | UI/UX Designer <br /> Tech Blogger </span>
+                </div>
+            </div>
+
+            <div className="beech overflow-y-auto pt-48 top-48 overflow-x-hidden no-scrollbar text-white">
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+                <div className="">
+                    <h6 className='text-white text-xl'>Residence</h6>
+                    <span>Pakistan</span>
+                </div>
+                <div className="">
+                    <h6>City</h6>
+                    <span>Peshawar</span>
+                </div>
+                <div className="">
+                    <h6>Age</h6>
+                    <span>22</span>
+                </div>
+            </div>
+
+
+            <div className="footerr absolute flex justify-center space-x-6 text-xl items-center bottom-0 h-10 w-full bg-gray-700">
+                <div className=""><FaGithub /></div>
+                <div className=""><FaTwitter /></div>
+                <div className=""><FaFacebook /></div>
+                <div className=""><FaLinkedin /></div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Intro
