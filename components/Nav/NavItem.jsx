9ecc5161135cad6dc41595dd9dab632ec5227ab7
osamajavaid <18pwcse1638@uetpeshawar.edu.pk>
2023-03-11 23:09:58 +0500

    changes

--- components/Nav/NavItem.jsx
+++ components/Nav/NavItem.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 const NavItem = ({ NavIcon, NavText, NavRoute, setOpen }) => {
     const router = useRouter();
-    const className = router.asPath === `${NavRoute}` ? "bg-yellow-500 text-slate-900" : '';
+    const className = router.asPath === `${NavRoute}` ? "text-gray-900 bg-yellow-500" : '';
 
     return (
         <Link onClick={(e) => setOpen(false)} href={NavRoute} className={`${className} flex items-center px-2 hover:bg-yellow-500 text-gray-400 hover:text-gray-900 hover:font-bold py-1 font-bold space-x-4 text-lg`}>
