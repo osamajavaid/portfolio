322bc8ac6f8945c1292975766ba8d0f815234bc1
osamajavaid <18pwcse1638@uetpeshawar.edu.pk>
2023-06-11 23:59:02 +0500

    resume added

--- components/Common/Intro/Download.jsx
+++ components/Common/Intro/Download.jsx
@@ -1,10 +1,10 @@
 const Download = ({ icon }) => {
     return (
         <>
-            <div id="scroll-intro" className="flex flex-row text-gray-500 items-center gap-x-4 py-8 pt-4">
+            <a href="/osamajavaid-resume.pdf" download id="scroll-intro" className="flex flex-row text-gray-500 items-center gap-x-4 py-8 pt-4">
                 <span className="text-white">Download Resume</span>
                 <span>{icon}</span>
-            </div>
+            </a>
             <div className="">
                 {/* f */}
             </div>
