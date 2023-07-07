daf551aa78edafbf5c14f3e442380050870c9011
osamajavaid <18pwcse1638@uetpeshawar.edu.pk>
2023-07-07 12:27:47 +0500

    card layout added in edu exp

--- components/Background/Edu_Card.jsx
+++ components/Background/Edu_Card.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import CardLayout from '../Common/CardLayout'
 
 const Edu_Card = ({ data }) => {
     return (
-        <div className="bg-[#20202a] px-8 py-10 mt-4 md:mr-12">
+        // <CardLayout>
+        <div className="backdrop-filter backdrop-blur-xl bg-[#20202a]/95 hover:bg-[#20202a]/90 transition bg-[#20202a] px-8 py-10 mt-4 md:mr-12">
             <div className="flex justify-between">
                 <div className="flex-initial text-[17px] text-white font-medium">{data.title}</div>
                 <div className="flex justify-items-center justify-center text-gray-500 bg-[#161a23] ml-2 w-32 lg:w-28 h-10 text-xs rounded-full py-3 font-normal opacity-50 text-center">
@@ -17,6 +19,7 @@ const Edu_Card = ({ data }) => {
                 {data.detail}
             </div>
         </div>
+        // </CardLayout>
     )
 }
 
--- components/Background/Exp_Card.jsx
+++ components/Background/Exp_Card.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
+import CardLayout from '../Common/CardLayout'
 
 const Exp_Card = ({ data }) => {
     return (
-        <div className="bg-[#20202a] px-8 py-10 mt-4 ">
-            <div className="flex-initial text-[17px] text-white font-medium">{data.title}</div>
-            <div className="text-sm text-gray-500 font-normal italic mt-1 ">
-                {data.role}
+        <CardLayout>
+            <div className="backdrop-filter backdrop-blur-xl bg-[#20202a]/95 hover:bg-[#20202a]/90 transition bg-[#20202a] px-8 py-10 mt-4 ">
+                <div className="flex-initial text-[17px] text-white font-medium">{data.title}</div>
+                <div className="text-sm text-gray-500 font-normal italic mt-1 ">
+                    {data.role}
+                </div>
+                <div className="text-gray-500 text-xs opacity-50 font-normal italic">
+                    <a href={`${data.url}`} target="_blank" rel="noreferrer">
+                        {data.url}
+                    </a>
+                </div>
+                <div className="text-gray-500 text-sm font-normal mt-4 text-justify">
+                    {data.desc}
+                </div>
+                <div className="flex justify-between text-gray-500 bg-[#161a23] w-full h-auto text-xs rounded-full py-3 px-6 mt-4 font-normal opacity-50">
+                    <div>{data.year}</div> <div>{data.location}</div>
+                </div>
             </div>
-            <div className="text-gray-500 text-xs opacity-50 font-normal italic">
-                <a href={`${data.url}`} target="_blank" rel="noreferrer">
-                    {data.url}
-                </a>
-            </div>
-            <div className="text-gray-500 text-sm font-normal mt-4 text-justify">
-                {data.desc}
-            </div>
-            <div className="flex justify-between text-gray-500 bg-[#161a23] w-full h-auto text-xs rounded-full py-3 px-6 mt-4 font-normal opacity-50">
-                <div>{data.year}</div> <div>{data.location}</div>
-            </div>
-        </div>
+        </CardLayout>
     )
 }
 
