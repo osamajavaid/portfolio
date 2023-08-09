0635b71f1aaf51af0d11c06ba7ee21e1fcd145c4
osamajavaid <iosamajavaid@gmail.com>
2023-08-10 00:24:14 +0500

    deleting graytheme branch

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@ import Recommendations from '../components/HomeComponents/Recommendations/Recomm
 import ClientReviews from '../components/HomeComponents/ClientReviews/ClientReviews';
 const home = () => {
     return (
-        <div className="HomePage -z-10">
+        <div className="Home-Page -z-10">
             <Banner />
             <MyExpertise />
             <Recommendations />
